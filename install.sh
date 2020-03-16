532cbd1c8d49e8a9ed3a2abae4da289d92c7c806
火烧兔子 <874811226@qq.com>
2020-03-16 11:45:05 +0800

    version

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# version
+DOCKER_COMPOSE_VERSION="1.23.2"
+
 # yum
 yum update
 
@@ -19,6 +22,9 @@ systemctl start docker
 systemctl enable docker
 
 # Docker-Compose
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+
+# Workspace
+docker-compose up -d
