7d325200822cc32d60fd7873b00d79fc46453e98
火烧兔子 <874811226@qq.com>
2020-03-16 11:41:05 +0800

    installshell

---
+++ install.sh
@@ -0,0 +1,24 @@
+# yum
+yum update
+
+# Git
+yum install -y git
+
+# Docker
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
+systemctl enable docker
+
+# Docker-Compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
