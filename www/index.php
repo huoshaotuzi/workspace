47a0b4a53ee7a06b93912ba9c8eca8517c7466fc
火烧兔子 <874811226@qq.com>
2020-05-30 20:43:58 +0800

    多版本

--- www/index.php
+++ www/index.php
@@ -1,21 +1,2 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP workspace</title>
-</head>
-<body>
-<h1 align="center">Docker-compose</h1>
-<h3 align="center">PHP+MySQL+Redis</h3>
-<h4 align="center">author：<u>huoshaotuzi</u>，email：<u>huotu@huotuyouxi.com</u></h4>
-<p align="center"><a href="https://github.com/huoshaotuzi/workspace" target="_blank">Github Page</a></p>
-
-<p align="center"><?php echo "let's enjoy!" ?></p>
-<h3 align="center">Extension</h3>
-
-<p><?php echo implode(',', get_loaded_extensions()); ?></p>
-</body>
-</html>
+<?php
+phpinfo();
